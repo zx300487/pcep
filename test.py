fab26085fe7b57bb207016e829cfb57b540366f4
zx300487 <zx300487@ou.ac.uk>
2022-02-03 13:32:58 +0000

    Latest session

---
+++ test.py
@@ -0,0 +1,15 @@
+# Cuboid volume and surface area
+
+
+a = int(3)
+b = int(4)
+c = int(5)
+
+volume = a * b * c # Formula for volume: V = abc
+
+print (str(volume) + ("cm cubed"))
+
+surface_area = (2 * (a * b)) + (2 * (b * c)) + (2 * (a * c)) # Formula for surface area S = 2ab + 2bc +2ac
+
+print (str(surface_area) + ("cm squared"))
+
